a902a4c94dca8b6111c0b7cf01c3287fa6180283
Sanya Saeed <120942171+Sanya2911@users.noreply.github.com>
2023-11-23 18:55:06 +0530

    Add files via upload

--- credit_card_fraud_detection/credit_card_fraud_detection(task_3_codsoft).py
+++ credit_card_fraud_detection/credit_card_fraud_detection(task_3_codsoft).py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1IVl0KXocqUqJuifUjWI0tn1W3VSsbJHl
 """
 
+#importing the libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -27,38 +28,17 @@ X = fraudtrain_data.drop(['trans_date_trans_time', 'merchant', 'category', 'firs
 y = fraudtest_data['is_fraud']
 print(X.shape)
 print(y.shape)
-
-# Build and train the Random Forest classifier
-rf_classifier = RandomForestClassifier(random_state=42)
-rf_classifier.fit(X, y)
-
-# Make predictions
-rf_predictions = rf_classifier.predict(X)
-
-# Evaluate the model
-print("Random Forest Accuracy:", accuracy_score(y, rf_predictions))
-print("Random Forest Classification Report:")
-print(classification_report(y, rf_predictions))
-print("Random Forest Confusion Matrix:")
-print(confusion_matrix(y, rf_predictions))
-
 print(fraudtrain_data.shape)
 print(fraudtest_data.shape)
 
-# Prepare features and target
-X = fraudtrain_data.drop(['trans_date_trans_time', 'merchant', 'category', 'first', 'last', 'gender', 'street', 'city', 'state', 'job', 'trans_num', 'dob'], axis=1)
-y = fraudtest_data['is_fraud']
-print(X.shape)
-print(y.shape)
-
 # Build and train the Random Forest classifier
 rf_classifier = RandomForestClassifier(random_state=42)
 rf_classifier.fit(X, y)
 
-# Make predictions
+# Make predictions from random forests
 rf_predictions = rf_classifier.predict(X)
 
-# Evaluate the model
+#Evaluating the model
 print("Random Forest Accuracy:", accuracy_score(y, rf_predictions))
 print("Random Forest Classification Report:")
 print(classification_report(y, rf_predictions))
